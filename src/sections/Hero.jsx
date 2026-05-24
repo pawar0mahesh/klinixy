3896956aa4caa005a0fd76e2cad3420bb4b33dd2
pawar0mahesh <gss9cmaheshpawar@gmail.com>
2026-05-24 14:24:13 +0530

    image responsiveness

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -74,12 +74,32 @@ function Hero() {
 
 
 
-      <img
-       src={bglogo}
-       alt="Logo Background"
-       className="absolute w-[60%] h-auto opacity-10 blur-[2px] "
-       style={{ top: '60%', left: '60%', transform: 'translate(-50%, -50%) ' }}
-       />
+    <img
+  src={bglogo}
+  alt="Logo Background"
+  className="
+    absolute
+    top-[35%]
+    sm:top-[50%]
+    md:top-[55%]
+
+    left-[50%]
+    sm:left-[52%]
+    md:left-[60%]
+
+    -translate-x-1/2
+    -translate-y-1/2
+
+    w-[85%]
+    sm:w-[70%]
+    md:w-[60%]
+
+    h-auto
+    opacity-10
+    blur-[2px]
+    z-0
+  "
+/>
 
       {/* Right Image */}
       <div className="flex-1 flex justify-center ">
@@ -89,18 +109,21 @@ function Hero() {
   alt="Healthcare"
   className="
     w-full
-    max-w-xs
-    sm:max-w-md
-    md:max-w-2xl
-    lg:max-w-4xl
+    max-w-sm
+    sm:max-w-lg
+    md:max-w-3xl
+    lg:max-w-6xl
     h-auto
     z-[1]
     mx-auto
+    scale-135
+    md:scale-110
     translate-x-0
     md:translate-x-10
-    lg:translate-x-15
+    lg:translate-x-16
   "
 />
+
 
       </div>
 
